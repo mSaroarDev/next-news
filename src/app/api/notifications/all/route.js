a05407f2c3bc51f65ba83f09ba0006ee957446ac
mSaroarDev <msaroa.dev@gmail.com>
2024-10-01 23:06:15 +0600

    api fixed

--- src/app/api/notifications/all/route.js
+++ src/app/api/notifications/all/route.js
@@ -5,9 +5,9 @@ export async function GET(req, res) {
   try {
     const data = await notificationModel.find().sort({ _id: -1 }).limit(20);
 
-    return NextResponse.json({ msg: "success", data });
+    return NextResponse.json({ msg: "success", data }, { status: 200 });
   } catch (error) {
     console.log("error in api/notifications/all", error);
-    return NextResponse.json({ msg: "success", data });
+    return NextResponse.json({ msg: "success", error }, { status: 500 });
   }
 }
