e29559331af6e99ea8cadc81c07b49fedf5af11b
mSaroarDev <msaroa.dev@gmail.com>
2024-10-12 23:50:56 +0600

    featured post indicator added

--- src/components/PostRow.jsx
+++ src/components/PostRow.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const PostRow = ({ data, userData }) => {
   return (
     <>
-      <div className="h-auto lg:h-[130px] w-full mb-2 box-shadow flex flex-col md:flex-row relative">
+      <div className="h-auto lg:h-[130px] w-full mb-2 box-shadow flex flex-col md:flex-row relative overflow-hidden">
         <div className="w-full h-[200px] md:h-full md:w-44">
           <img
             src={
@@ -20,13 +20,13 @@ const PostRow = ({ data, userData }) => {
         <div className="flex-grow px-4 py-2 flex flex-col justify-between">
           <H5 className="text-base font-bold mb-2" text={data?.title} />
           <p className="text-sm text-gray">
-            in category: {data?.category?.categoryName}{" "}
+            in category: <span className="text-brand font-semibold">{data?.category?.categoryName}</span> {" "}
           </p>
           <p className="text-sm text-gray">
             posted on: 24 Jun, 2024 - 08.00 PM
           </p>
           <p className="text-sm text-gray">posted by: Saroar</p>
-          <p className="text-sm text-gray">comments: 45</p>
+          <p className="text-sm text-gray">comments: {data?.comments?.length}</p>
         </div>
 
         {userData?.id == data?.created_by?._id && (
@@ -36,6 +36,12 @@ const PostRow = ({ data, userData }) => {
           >
             <SquarePen className="w-5 h-5 hover:text-brand transition-all duration-200" />
           </Link>
+        )}
+
+        {data?.isFeatured === true && (
+          <div className="absolute w-40 h-fit top-0 -left-9 bg-black text-white -rotate-45 px-7 py-1 text-xs">
+            Featured
+          </div>
         )}
       </div>
     </>
